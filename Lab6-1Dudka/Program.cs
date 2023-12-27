d5a6ea1f80a83365ad2a100bea560e1a9ee6aa19
B3dRandoms <bdrandoms0@gmail.com>
2023-12-28 00:41:32 +0200

    Initial commit

---
+++ Lab6-1Dudka/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите первое натуральное число:");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе натуральное число:");
+int number2 = int.Parse(Console.ReadLine());
+
+if (IsPalindrome(number1) || IsPalindrome(number2)) 
+{
+    Console.WriteLine("Хотя бы одно из чисел является палиндромом");
+}
+else
+{
+    Console.WriteLine("Ни одно из чисел не является палиндромом");
+}
+Console.ReadLine();
+static bool IsPalindrome(int number)
+{
+    string str = number.ToString(); 
+
+    for (int i = 0; i < str.Length / 2; i++)
+    {
+        if (str[i] != str[str.Length - 1 - i]) 
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
